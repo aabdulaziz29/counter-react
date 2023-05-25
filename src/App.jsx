e825cf5a2d24fbec5a0639fa35b2e1b1fc003bea
Abdulaziz <aabduvakilov98@gmail.com>
2023-05-25 14:39:49 +0500

    counter is working

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Minus
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Plus +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
